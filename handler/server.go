3cc8098bbecffd4a4e0511fbb2c982d32f75b916
husnutapan <husnu.tapan@gmail.com>
2020-05-31 21:28:52 +0300

    up server

--- handler/server.go
+++ handler/server.go
@@ -1,15 +1,17 @@
 package handler
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/husnutapan/go-build-microservice/utility"
 	"net/http"
 )
 
 func (svr *ServerInformations) UpServer() {
+	prepareRoutings(svr)
 	http.ListenAndServe(":8080", svr.Router)
 }
 
-func (s *ServerInformations) prepareRoutings() {
-	s.Router.HandleFunc("/", utility.AddHeaderToJSON(s.Home))
-
+func prepareRoutings(svr *ServerInformations) {
+	svr.Router = mux.NewRouter()
+	svr.Router.HandleFunc("/", utility.AddHeaderToJSON(svr.Home)).Methods("GET")
 }
